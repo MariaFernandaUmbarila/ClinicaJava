06bbe13dc3f9f1486a0c8423a71986c93dd18613
fernandaumb <umbarila.maria@locatelcolombia.com>
2023-07-21 16:50:59 -0500

    Commit inicial

---
+++ src/main/kotlin/com/clinicajava/clinicajava/ClinicaJavaApplication.kt
@@ -0,0 +1,11 @@
+package com.clinicajava.clinicajava
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ClinicaJavaApplication
+
+fun main(args: Array<String>) {
+    runApplication<ClinicaJavaApplication>(*args)
+}
